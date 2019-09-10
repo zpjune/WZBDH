cc58a1aa969c148d3eb667925523b928982b79a5
zpjune <1084013577@qq.com>
2019-09-10 15:39:29 +0800

    修改

---
+++ LHSM.WRI.ObjSapForRemoting/LoadWZ/ClsDataLoadBAOBIAO.cs
@@ -0,0 +1,52 @@
+﻿using LHSM.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LHSM.HB.ObjSapForRemoting
+{
+   public class ClsDataLoadBAOBIAO 
+    {
+        //积压物资统计报表
+        //库存物资统计报表
+        //数据库连接
+        private static ClsDBConnection m_Conn = null;
+
+        /// <summary>
+        /// 存储sql字符串变量
+        /// </summary>
+        private StringBuilder strBuilder = new StringBuilder();
+
+        public static void SAPLoadData(ClsSAPDataParameter p_para)
+        {
+            bool Result = true;
+            string dldate = p_para.Sap_AEDAT;
+            m_Conn = ClsUtility.GetConn();
+            string sqlDelete = "begin DELETE FROM CONVERT_SWKCDETAILCKJE;commit; end;";
+            if (m_Conn.ExecuteSql(sqlDelete))
+            {
+                string sql = "BEGIN  INSERT INTO CONVERT_SWKCDETAILCKJE(WERKS,WERKS_NAME,LGORT,MATNR,GESME,ZCKTZD,ZITEM,DLNAME,PMNAME,DLDATE,CKJE) ";
+                sql += "select A.WERKS,D.DW_NAME,A.LGORT,A.MATNR,A.ZFHSL,A.ZCKTZD,A.ZCITEM,C.DLNAME,C.PMNAME,'" + dldate + "',ROUND(E.SALK3/LBKUM*A.ZFHSL,2)";
+                sql += @" from  ZC10MMDG078 A 
+                    JOIN WZ_DW D ON A.WERKS = D.DW_CODE
+                    JOIN MBEW E ON E.BWKEY = A.WERKS AND E.MATNR = A.MATNR AND E.SALK3 > 0
+                    JOIN WZ_WLZ C ON C.PMCODE = A.MATKL
+                    WHERE A.ZSTATUS = '03'; COMMIT; END ;";
+                Result = m_Conn.ExecuteSql(sql);
+                if (Result)
+                {
+                    ClsErrorLogInfo.WriteSapLog("1", "CONVERT_SWKCDETAILCKJE", "ALL", p_para.Sap_AEDAT, "插入CONVERT_SWKCDETAILCKJE表成功");
+                }
+                else
+                {
+                    ClsErrorLogInfo.WriteSapLog("1", "CONVERT_SWKCDETAILCKJE", "ALL", p_para.Sap_AEDAT, "插入CONVERT_SWKCDETAILCKJE表失败");
+                }
+            }
+            else {
+                ClsErrorLogInfo.WriteSapLog("1", "CONVERT_SWKCDETAILCKJE", "ALL", p_para.Sap_AEDAT, "删除CONVERT_SWKCDETAILCKJE表失败");
+            }
+            
+        }
+    }
+}
--- LHSM.WRI.ObjSapForRemoting/LoadWZ/ClsDataLoadSWKC.cs
+++ LHSM.WRI.ObjSapForRemoting/LoadWZ/ClsDataLoadSWKC.cs
@@ -20,12 +20,14 @@ namespace LHSM.HB.ObjSapForRemoting
         /// 存储sql字符串变量
         /// </summary>
         private StringBuilder strBuilder = new StringBuilder();
+        private StringBuilder strDetail = new StringBuilder();
         public bool SAPLoadData(ClsSAPDataParameter p_para)
         {
             string dldate = p_para.Sap_AEDAT;
             bool Result = true;
             m_Conn = ClsUtility.GetConn();
             strBuilder.Append(" Begin ");
+            strDetail.Append(" Begin ");
             try
             {
                 string sqlLQUA = @"select SUM(t.GESME) GESME,t.MATNR,t.WERKS,t.LGORT,substr(t.LGPLA,1,4) LGPLA,b.DW_NAME,c.KCDD_NAME
@@ -33,11 +35,12 @@ namespace LHSM.HB.ObjSapForRemoting
                                     join WZ_DW b ON b.DW_CODE=t.Werks
                                     join WZ_KCDD c ON c.DWCODE=t.WERKS AND c.KCDD_CODE=t.LGORT
                                     where  regexp_like(lgpla,'^[0-9]+[0-9]$') and matnr  in (select matnr from ZC10MMDG085B) 
-                                     GROUP BY t.MATNR,t.WERKS,t.LGORT,substr(t.LGPLA,1,4),b.DW_NAME,c.KCDD_NAME  "; //查询lqua表库存，以上架后的(lgpla为数字的，并且matnr 在085b里有的)
+                                     GROUP BY t.MATNR,t.WERKS,t.LGORT,substr(t.LGPLA,1,4),b.DW_NAME,c.KCDD_NAME  "; //查询lqua表库存，以上架后的(lgpla为数字的，并且matnr 在085b里有的) 
+                //regexp_like(lgpla,'^[0-9]+[0-9]$')过滤不是数字的
                 DataTable dtLQUA = m_Conn.GetSqlResultToDt(sqlLQUA);
                 if (dtLQUA != null && dtLQUA.Rows.Count > 0)
                 {
-                    string sqlWLZ = @"  select  d.MATNR,d.MATKL,d.MAKTX,e.PMNAME,e.JBJLDW 
+                    string sqlWLZ = @"  select  d.MATNR,d.MATKL,d.MAKTX,e.PMNAME,e.JBJLDW ,e.DLNAME
                                     FROM   MARA d 
                                     join WZ_WLZ e on e.PMCODE=d.MATKL ";//查询物料组基本计量单位，物料描述
                     DataTable dtWLZ= m_Conn.GetSqlResultToDt(sqlWLZ);
@@ -46,6 +49,7 @@ namespace LHSM.HB.ObjSapForRemoting
                                          WHERE GESME>0 and ZDHTZD is not null
                                           order by  MATNR,WERKS,LGORT,substr(LGPLA,1,4) ASC,ERDAT  DESC";//查询085B 因为这个表有仓位//倒查是否有积压
                     DataTable dt085B = m_Conn.GetSqlResultToDt(sql085B);
+                    int a = 0;
                     foreach (DataRow rowLQUA in dtLQUA.Rows)
                     { 
                         double GESME = double.Parse(rowLQUA["GESME"].ToString());
@@ -61,13 +65,40 @@ namespace LHSM.HB.ObjSapForRemoting
                                 totalGESME = totalGESME + double.Parse(item["GESME"].ToString());
                                 if (GESME > totalGESME)
                                 {
+                                    if (arrWLZ.Length>0) {
+                                        strDetail.Append(" INSERT INTO CONVERT_SWKCDETAIL(WERKS,WERKS_NAME,LGORT,MATNR,GESME,ERDATE,DLNAME,PMNAME,DLDATE) VALUES(  ");//插入明细表，为后面算账务金额做基础
+                                        strDetail.Append(" '" + rowLQUA["WERKS"].ToString() + "', ");
+                                        strDetail.Append(" '" + rowLQUA["DW_NAME"]?.ToString() + "', ");
+                                        strDetail.Append(" '" + rowLQUA["LGORT"]?.ToString() + "', ");
+                                        strDetail.Append(" '" + item["MATNR"]?.ToString() + "', ");
+                                        strDetail.Append(" '" + item["GESME"]?.ToString() + "', ");
+                                        strDetail.Append(" '" + item["ERDAT"]?.ToString() + "', ");
+                                        strDetail.Append(" '" + arrWLZ[0]["DLNAME"]?.ToString() + "', ");
+                                        strDetail.Append(" '" + arrWLZ[0]["PMNAME"]?.ToString() + "', ");
+                                        strDetail.Append(" '" + dldate + "'); ");
+                                    }
                                     continue;//如果当前累计数量和小于库存，继续循环
                                 }
                                 else {
+                                    //插入明细表，为后面算账务金额做基础
+                                    if (arrWLZ.Length > 0)
+                                    {
+                                        strDetail.Append(" INSERT INTO CONVERT_SWKCDETAIL(WERKS,WERKS_NAME,LGORT,MATNR,GESME,ERDATE,DLNAME,PMNAME,DLDATE) VALUES(  ");//插入明细表，为后面算账务金额做基础
+                                        strDetail.Append(" '" + rowLQUA["WERKS"].ToString() + "', ");
+                                        strDetail.Append(" '" + rowLQUA["DW_NAME"]?.ToString() + "', ");
+                                        strDetail.Append(" '" + rowLQUA["LGORT"]?.ToString() + "', ");
+                                        strDetail.Append(" '" + item["MATNR"]?.ToString() + "', ");
+                                        strDetail.Append(" '" + item["GESME"]?.ToString() + "', ");
+                                        strDetail.Append(" '" + item["ERDAT"]?.ToString() + "', ");
+                                        strDetail.Append(" '" + arrWLZ[0]["DLNAME"]?.ToString() + "', ");
+                                        strDetail.Append(" '" + arrWLZ[0]["PMNAME"]?.ToString() + "', ");
+                                        strDetail.Append(" '" + dldate + "'); ");
+                                    }
+
 
                                     //根据mard库存按工厂、物料码、库存地点，分组，回找入库通知单，直到入库通知单数量和大于或等于mard数量
                                     //将近期入库通知单收货数量小于库存的都插入实物库存表CONVERT_SWKC
-                                    strBuilder.Append(" begin INSERT INTO CONVERT_SWKC (WERKS,ZDHTZD,ZITEM,MATKL,MATNR,MAKTX," +
+                                    strBuilder.Append("  INSERT INTO CONVERT_SWKC (WERKS,ZDHTZD,ZITEM,MATKL,MATNR,MAKTX," +
                                        "MEINS,GESME,LGORT,LGPLA,ERDAT,WERKS_NAME,LGORT_NAME,ZSTATUS,YXQ,DLDATE) VALUES(");
                                     strBuilder.Append(" '" + rowLQUA["WERKS"].ToString() + "',");
                                     strBuilder.Append(" '" + item["ZDHTZD"] + "',");
@@ -99,7 +130,7 @@ namespace LHSM.HB.ObjSapForRemoting
                                     strBuilder.Append(" '" + item["ERDAT"]?.ToString() + "',");
                                     strBuilder.Append(" '" + rowLQUA["DW_NAME"] + "',");
                                     strBuilder.AppendLine(" '" + rowLQUA["KCDD_NAME"].ToString().Replace('(', ' ').Replace(')', ' ').Replace('）', ' ').Replace('（', ' ').Replace('\'', ' ') +
-                                        "','04','','"+ dldate + "'); commit; end; ");
+                                        "','04','','"+ dldate + "');  ");
 
                                 }
                             }
@@ -108,9 +139,18 @@ namespace LHSM.HB.ObjSapForRemoting
                     }
                     if (strBuilder.Length > 8)
                     {
-                        strBuilder.Append(" end ;");
-                        if (ClsUtility.ExecuteSqlToDb(" begin delete from CONVERT_SWKC; commit; end; "))
+                        strBuilder.Append(" COMMIT; end ; ");
+                        strDetail.Append(" COMMIT; end ; ");
+                        if (m_Conn.ExecuteSql(" begin delete from CONVERT_SWKC;DELETE FROM CONVERT_SWKCDETAIL; commit; end; "))
                         {
+                            Result = m_Conn.ExecuteSql(strDetail.ToString());//执行失败竟然也返回true
+                            if (Result)
+                            {
+                                ClsErrorLogInfo.WriteSapLog("1", "CONVERT_SWKCDETAIL", "ALL", p_para.Sap_AEDAT, "插入CONVERT_SWKCDETAIL失败,应该是sql有问题，可能含特殊字符了，");
+                            }
+                            else {
+                                ClsErrorLogInfo.WriteSapLog("1", "CONVERT_SWKCDETAIL", "ALL", p_para.Sap_AEDAT, "插入CONVERT_SWKCDETAIL失败,应该是sql有问题，可能含特殊字符了，");
+                            }
                             Result = ClsUtility.ExecuteSqlToDb(strBuilder.ToString());//执行失败竟然也返回true
                             if (Result)
                             {
@@ -132,8 +172,8 @@ namespace LHSM.HB.ObjSapForRemoting
                                                      join WZ_KCDD c ON c.DWCODE=t.WERKS AND c.KCDD_CODE=t.LGORT
                                                     join    MARA d on d.matnr=t.matnr
                                                     join WZ_WLZ e on e.PMCODE=d.MATKL
-                                                     where ZSTATUS ='03'; commit; end; ");
-                                    Result = ClsUtility.ExecuteSqlToDb(strBuilder.ToString());
+                                                     where ZSTATUS ='03'; commit; end; ");//插入质检状态的入库单作为另一部分库存
+                                    Result = m_Conn.ExecuteSql(strBuilder.ToString());
                                     if (Result) {
                                         ClsErrorLogInfo.WriteSapLog("1", "CONVERT_SWKC", "ALL", p_para.Sap_AEDAT, "插入CONVERT_SWKC表质检部分成功");
                                     }
@@ -141,6 +181,12 @@ namespace LHSM.HB.ObjSapForRemoting
                                         ClsErrorLogInfo.WriteSapLog("1", "CONVERT_SWKC", "ALL", p_para.Sap_AEDAT, "插入CONVERT_SWKC表质检部分失败");
                                     }
 
+                                }
+                                string sqlupdateje = @"UPDATE CONVERT_SWKCDETAIL A SET A.SCJE=(select ROUND(B.SALK3/LBKUM*A.GESME,2) 
+                                                        FROM MBEW B
+                                                        WHERE B.SALK3>0 AND A.MATNR=B.MATNR AND  A.WERKS=B.BWKEY)";
+                                if (!m_Conn.ExecuteSql(sqlupdateje)) {
+                                    ClsErrorLogInfo.WriteSapLog("1", "CONVERT_SWKCDETAIL", "ALL", p_para.Sap_AEDAT, "更新CONVERT_SWKCDETAIL表金额失败");
                                 }
                             }
                             else
@@ -150,7 +196,7 @@ namespace LHSM.HB.ObjSapForRemoting
                         }
                     }
                 }
-                
+                ClsDataLoadBAOBIAO.SAPLoadData(p_para);//下架未过账库存金额模型
             }
             catch (Exception ex)
             {
