ccf19c5e98748773a55075dd8db1ae4696a143ea
zpjune <1084013577@qq.com>
2019-10-14 15:19:42 +0800

    修改

--- LHSM.WRI.ObjSapForRemoting/LoadWZ/ClsDataLoadZWKC.cs
+++ LHSM.WRI.ObjSapForRemoting/LoadWZ/ClsDataLoadZWKC.cs
@@ -26,50 +26,17 @@ namespace LHSM.HB.ObjSapForRemoting
             DataTable dtSWKC = new DataTable();
             try
             {
-                string strMBEW = "SELECT COUNT(*) FROM MBEW";
                 string stedate = p_para.Sap_AEDAT.Substring(0, 6);
-                DataTable dtMBEW= m_Conn.GetSqlResultToDt(strMBEW);
-                
-                if (dtMBEW != null && dtMBEW.Rows.Count > 0 && dtMBEW.Rows[0][0].ToString() != "0")
-                {//不是第一次转换，先删除操作 ，再insert
-                    string sql= @"SELECT A.BWKEY,D.DW_NAME,A.SALK3,C.DLCODE,C.DLNAME,C.ZLCODE,C.ZLNAME,C.XLCODE,C.XLNAME,B.MATKL,C.PMNAME
-                                    FROM MBEW A 
+                //是第一次转换，先删除操作 ，再insert
+                //  插入CONVERT_ZWKC表模型
+                string strSqlEkko = @" begin delete from CONVERT_ZWKC; INSERT INTO  CONVERT_ZWKC (BWKEY,BWKEY_NAME,MATNR,SALK3,DLCODE,DLNAME,ZLCODE,ZLNAME,XLCODE,XLNAME,MATKL,PMNAME,LBKUM,DANJIA,DLDATE)
+                                    SELECT   A.BWKEY,D.DW_NAME,A.MATNR,A.SALK3,C.DLCODE,C.DLNAME,C.ZLCODE,C.ZLNAME,C.XLCODE,C.XLNAME,B.MATKL,C.PMNAME,A.LBKUM,A.SALK3/A.LBKUM,'" + p_para.Sap_AEDAT + "'";
+                strSqlEkko+= @" FROM MBEW A 
                                     JOIN MARA B ON A.MATNR=B.MATNR
                                     JOIN WZ_WLZ C ON C.PMCODE=B.MATKL
                                     JOIN WZ_DW D ON D.DW_CODE=A.BWKEY 
-                                    WHERE A.DLDATE>='" + stedate + "'";
-                    DataTable dtNew = m_Conn.GetSqlResultToDt(sql);
-                    strBuilder.Append("begin ");
-                    if (dtNew!=null&&dtNew.Rows.Count>0) {
-                        foreach (DataRow row in dtMBEW.Rows)
-                        {
-                            strBuilder.AppendLine("  delete from CONVERT_ZWKC where MANDT='" + row["MANDT"] + "' and MATNR='" + row["MATNR"] + "' and BWKEY='" + row["BWKEY"] + "' and  BWTAR='" + row["BWTAR"] + "' ;  ");
-                        }
-                        strBuilder.AppendLine(@" INSERT INTO  CONVERT_ZWKC
-                                                 (MANDT, MATNR, BWKEY, BWTAR, BWKEY_NAME, SALK3, DLCODE, DLNAME, ZLCODE, ZLNAME, XLCODE, XLNAME, MATKL, PMNAME)
-                                    SELECT A.BWKEY, D.DW_NAME, A.SALK3, C.DLCODE, C.DLNAME, C.ZLCODE, C.ZLNAME, C.XLCODE, C.XLNAME, B.MATKL, C.PMNAME
-                                    FROM MBEW A
-                                    JOIN MARA B ON A.MATNR = B.MATNR
-                                    JOIN WZ_WLZ C ON C.PMCODE = B.MATKL
-                                    JOIN WZ_DW D ON D.DW_CODE = A.BWKEY
-                                    WHERE A.DLDATE >= '" + stedate + "' ; commit; end");
-                        Result= m_Conn.ExecuteSql(strBuilder.ToString());
-                    }
-                    strBuilder.Length = 0;//清空
-                }
-                else {
-                    //是第一次转换，先删除操作 ，再insert
-                    //  插入CONVERT_ZWKC表模型
-                    string strSqlEkko = @" begin INSERT INTO  CONVERT_ZWKC
-                                                 (MANDT,MATNR,BWKEY,BWTAR,BWKEY_NAME,SALK3,DLCODE,DLNAME,ZLCODE,ZLNAME,XLCODE,XLNAME,MATKL,PMNAME)
-                                    SELECT A.BWKEY,D.DW_NAME,A.SALK3,C.DLCODE,C.DLNAME,C.ZLCODE,C.ZLNAME,C.XLCODE,C.XLNAME,B.MATKL,C.PMNAME
-                                    FROM MBEW A 
-                                    JOIN MARA B ON A.MATNR=B.MATNR
-                                    JOIN WZ_WLZ C ON C.PMCODE=B.MATKL
-                                    JOIN WZ_DW D ON D.DW_CODE=A.BWKEY 
-                                    WHERE A.DLDATE>='" + stedate + "' ; commit;end ;";
+                                     ; commit;end ;";
                     Result = m_Conn.ExecuteSql(strSqlEkko);
-                }
             }
             catch (Exception exception)
             {
