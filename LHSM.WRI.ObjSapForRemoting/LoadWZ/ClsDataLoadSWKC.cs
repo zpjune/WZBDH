018688207aa24ccf3fb53b37477ad46a98a0cda6
zpjune <1084013577@qq.com>
2019-12-29 09:32:42 +0800

    修改

--- LHSM.WRI.ObjSapForRemoting/LoadWZ/ClsDataLoadSWKC.cs
+++ LHSM.WRI.ObjSapForRemoting/LoadWZ/ClsDataLoadSWKC.cs
@@ -33,28 +33,130 @@ namespace LHSM.HB.ObjSapForRemoting
                     string sql = " begin insert into CONVERT_SWKC_RECORD SELECT * FROM CONVERT_SWKC ;COMMIT; END;  ";
                     m_Conn.ExecuteSql(sql);
                 }
-                string sqlLQUA = @"begin delete from CONVERT_SWKC where KCTYPE=0;
-                                    INSERT INTO CONVERT_SWKC (WERKS,MATKL,MATNR,MAKTX,
-                                     MEINS,GESME,LGORT,LGPLA,ERDAT,WERKS_NAME,LGORT_NAME,ZSTATUS,YXQ,DLDATE,KCTYPE)
+                //string sqlLQUA = @"begin delete from CONVERT_SWKC where KCTYPE=0;
+                //                    INSERT INTO CONVERT_SWKC (WERKS,MATKL,MATNR,MAKTX,
+                //                     MEINS,GESME,LGORT,LGPLA,ERDAT,WERKS_NAME,LGORT_NAME,ZSTATUS,YXQ,DLDATE,KCTYPE)
+                //                     SELECT A.WERKS,C.MATKL,A.MATNR,C.MAKTX,
+                //                     F.JBJLDW,A.GESME,A.LGORT,substr(A.LGPLA,0,4),case when SUBSTR(A.WDATU, 0, 8)='00000000' then A.BDATU ELSE A.WDATU END,D.DW_NAME,E.KCDD_NAME,'04','',to_char(sysdate,'yyyymmdd'),0
+                //                     FROM LQUA A JOIN(
+                //                    select WERKS,MATNR,substr(lgpla,0,2) DKCODE,LGORT,max(BDATU) BDATU,max(LQNUM) LQNUM
+                //                    from LQUA where regexp_like(lgpla,'^[0-9]+[0-9]$') 
+                //                     group by werks,matnr,LGORT,substr(lgpla,0,2)) B ON A.WERKS=B.WERKS AND A.MATNR=B.MATNR AND substr(A.lgpla,0,2)=B.DKCODE AND A.LGORT=B.LGORT AND A.BDATU=B.BDATU AND A.LQNUM=B.LQNUM
+                //                     JOIN MARA C ON A.MATNR=C.MATNR
+                //                     join WZ_DW D ON D.DW_CODE=A.Werks
+                //                    LEFT join WZ_KCDD E ON E.DWCODE=A.WERKS AND E.KCDD_CODE=A.LGORT
+                //                    JOIN WZ_WLZ F ON F.PMCODE=C.MATKL ; COMMIT; end;"; //查询lqua表库存，lapla 为数字得，子查询是按物料编码取最新一条数据，主要取里面得BDATU,WDATU
+                //regexp_like(lgpla,'^[0-9]+[0-9]$')过滤lapla不是数字的
+                string sqlLQUA = @"
                                      SELECT A.WERKS,C.MATKL,A.MATNR,C.MAKTX,
-                                     F.JBJLDW,A.GESME,A.LGORT,substr(A.LGPLA,0,4),case when SUBSTR(A.WDATU, 0, 8)='00000000' then A.BDATU ELSE A.WDATU END,D.DW_NAME,E.KCDD_NAME,'04','',to_char(sysdate,'yyyymmdd'),0
+                                     F.JBJLDW MEINS,nvl(A.GESME,0)GESME,A.LGORT,substr(A.LGPLA,0,4)LGPLA,case when SUBSTR(A.WDATU, 0, 8)='00000000' then A.BDATU ELSE A.WDATU END ERDAT,
+                                        D.DW_NAME WERKS_NAME,E.KCDD_NAME,'04' ZSTATUS,''YXQ,to_char(sysdate,'yyyymmdd')DLDATE,0 KCTYPE
                                      FROM LQUA A JOIN(
-                                    select WERKS,MATNR,substr(lgpla,0,2) DKCODE,LGORT,max(BDATU) BDATU,max(LQNUM) LQNUM
+                                    select WERKS,MATNR,substr(lgpla,0,4) DKCODE,LGORT,max(LQNUM) LQNUM,max(BDATU)BDATU 
                                     from LQUA where regexp_like(lgpla,'^[0-9]+[0-9]$') 
-                                     group by werks,matnr,LGORT,substr(lgpla,0,2)) B ON A.WERKS=B.WERKS AND A.MATNR=B.MATNR AND substr(A.lgpla,0,2)=B.DKCODE AND A.LGORT=B.LGORT AND A.BDATU=B.BDATU AND A.LQNUM=B.LQNUM
+                                     group by werks,matnr,LGORT,substr(lgpla,0,4)) B ON A.WERKS=B.WERKS AND A.MATNR=B.MATNR AND 
+                                        substr(A.lgpla,0,4)=B.DKCODE AND A.LGORT=B.LGORT AND A.BDATU=B.BDATU AND A.LQNUM=B.LQNUM
                                      JOIN MARA C ON A.MATNR=C.MATNR
                                      join WZ_DW D ON D.DW_CODE=A.Werks
                                     LEFT join WZ_KCDD E ON E.DWCODE=A.WERKS AND E.KCDD_CODE=A.LGORT
-                                    JOIN WZ_WLZ F ON F.PMCODE=C.MATKL ; COMMIT; end;"; //查询lqua表库存，lapla 为数字得，子查询是按物料编码取最新一条数据，主要取里面得BDATU,WDATU
+                                    JOIN WZ_WLZ F ON F.PMCODE=C.MATKL "; //查询lqua表库存，lapla 为数字得，子查询是按物料编码取最新一条数据，主要取里面得BDATU,WDATU
                 //regexp_like(lgpla,'^[0-9]+[0-9]$')过滤lapla不是数字的
-                Result = m_Conn.ExecuteSql(sqlLQUA);
-                if (Result)
+
+                DataTable dt = m_Conn.GetSqlResultToDt(sqlLQUA);
+                if (dt!=null&&dt.Rows.Count>0)
                 {
-                    ClsErrorLogInfo.WriteSapLog("1", "CONVERT_SWKC", "ALL", DateTime.Now.ToString("yyyy-MM-dd"), "插入CONVERT_SWKC表上架部分成功");
+                    string sqlruku = @"select nvl(C.MENGE,0)MENGE,C.BUDAT_MKPF,A.MATNR,A.ZDHTZD, A.ZITEM,substr(B.LGPLA,0,4)LGPLA,A.WERKS,A.LGORT
+                                        from ZC10MMDG072 A
+                                        JOIN ZC10MMDG085B B ON A.ZDHTZD=B.ZDHTZD AND A.ZITEM=B.ZITEM  and regexp_like(B.lgpla,'^[0-9]+[0-9]$') 
+                                        JOIN MSEG C ON C.MBLNR=A.MBLNR AND C.ZEILE=A.ZEILE and C.BWART IN('105','101') 
+                                        WHERE exists (SELECT 1 FROM LQUA d WHERE d.MATNR=A.MATNR and regexp_like(d.lgpla,'^[0-9]+[0-9]$')  ) 
+                                        order by C.BUDAT_MKPF desc,a.matnr,a.zdhtzd,a.zitem ";
+                    DataTable dtruku = m_Conn.GetSqlResultToDt(sqlruku);
+                    StringBuilder sb = new StringBuilder();
+                    string sqlinsert = "INSERT INTO CONVERT_SWKC (WERKS,MATKL,MATNR,MAKTX,MEINS,GESME,LGORT,LGPLA,ERDAT,WERKS_NAME,LGORT_NAME,ZSTATUS,YXQ,DLDATE,KCTYPE)values ('";
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        DataRow[] rowsRK = dtruku.Select("MATNR='" + row["MATNR"] + "' and WERKS='" + row["WERKS"] + "' and  LGORT='" + row["LGORT"] + "' and LGPLA='" + row["LGPLA"] + "' ");
+                        if (rowsRK != null && rowsRK.Length > 0)
+                        {
+                            sb.AppendLine(sqlinsert);
+                            double sumSL = 0;
+                            int flag = 0;
+                            foreach (DataRow rowRK in rowsRK)
+                            {
+                                sumSL = sumSL+double.Parse(rowRK["MENGE"].ToString());
+                                if (sumSL >= double.Parse(row["GESME"].ToString()))
+                                {
+                                    sb.AppendLine(row["WERKS"].ToString()+"','");
+                                    sb.AppendLine(row["MATKL"].ToString() + "','");
+                                    sb.AppendLine(row["MATNR"].ToString() + "','");
+                                    sb.AppendLine(row["MAKTX"].ToString() + "','");
+                                    sb.AppendLine(row["MEINS"].ToString() + "','");
+                                    sb.AppendLine(row["GESME"].ToString() + "','");
+                                    sb.AppendLine(row["LGORT"].ToString() + "','");
+                                    sb.AppendLine(row["LGPLA"].ToString() + "','");
+                                    sb.AppendLine(rowRK["BUDAT_MKPF"].ToString() + "','");
+                                    sb.AppendLine(row["WERKS_NAME"].ToString() + "','");
+                                    sb.AppendLine(row["ZSTATUS"].ToString() + "','");
+                                    sb.AppendLine(row["YXQ"].ToString() + "','");
+                                    sb.AppendLine(row["DLDATE"].ToString() + "',");
+                                    sb.AppendLine(row["KCTYPE"].ToString() + ");");
+                                    flag = 1;
+                                }
+                            }
+                            if (flag==0) {
+                                sb.AppendLine(row["WERKS"].ToString() + "','");
+                                sb.AppendLine(row["MATKL"].ToString() + "','");
+                                sb.AppendLine(row["MATNR"].ToString() + "','");
+                                sb.AppendLine(row["MAKTX"].ToString() + "','");
+                                sb.AppendLine(row["MEINS"].ToString() + "','");
+                                sb.AppendLine(row["GESME"].ToString() + "','");
+                                sb.AppendLine(row["LGORT"].ToString() + "','");
+                                sb.AppendLine(row["LGPLA"].ToString() + "','");
+                                sb.AppendLine(row["ERDAT"].ToString() + "','");
+                                sb.AppendLine(row["WERKS_NAME"].ToString() + "','");
+                                sb.AppendLine(row["ZSTATUS"].ToString() + "','");
+                                sb.AppendLine(row["YXQ"].ToString() + "','");
+                                sb.AppendLine(row["DLDATE"].ToString() + "',");
+                                sb.AppendLine(row["KCTYPE"].ToString() + ");");
+                            }
+                        }
+                        else {
+                            sb.AppendLine(sqlinsert);
+                            sb.AppendLine(row["WERKS"].ToString() + "','");
+                            sb.AppendLine(row["MATKL"].ToString() + "','");
+                            sb.AppendLine(row["MATNR"].ToString() + "','");
+                            sb.AppendLine(row["MAKTX"].ToString() + "','");
+                            sb.AppendLine(row["MEINS"].ToString() + "','");
+                            sb.AppendLine(row["GESME"].ToString() + "','");
+                            sb.AppendLine(row["LGORT"].ToString() + "','");
+                            sb.AppendLine(row["LGPLA"].ToString() + "','");
+                            sb.AppendLine(row["ERDAT"].ToString() + "','");
+                            sb.AppendLine(row["WERKS_NAME"].ToString() + "','");
+                            sb.AppendLine(row["ZSTATUS"].ToString() + "','");
+                            sb.AppendLine(row["YXQ"].ToString() + "','");
+                            sb.AppendLine(row["DLDATE"].ToString() + "',");
+                            sb.AppendLine(row["KCTYPE"].ToString() + ");");
+                        }
+                        
+                    }
+
+                    string insertSWKC = @"begin delete from CONVERT_SWKC where KCTYPE=0;";
+                    insertSWKC += sb.ToString();
+                    insertSWKC += "  COMMIT; end;";
+                    Result = m_Conn.ExecuteSql(insertSWKC);
+                    if (Result) {
+                        ClsErrorLogInfo.WriteSapLog("1", "CONVERT_SWKC", "ALL", DateTime.Now.ToString("yyyy-MM-dd"), "插入CONVERT_SWKC表上架部分成功");
+                    }
+                    else
+                    {
+                        ClsErrorLogInfo.WriteSapLog("1", "CONVERT_SWKC", "ALL", DateTime.Now.ToString("yyyy-MM-dd"), "插入CONVERT_SWKC表上架部分失");
+                    }
+
                 }
                 else
                 {
-                    ClsErrorLogInfo.WriteSapLog("1", "CONVERT_SWKC", "ALL", DateTime.Now.ToString("yyyy-MM-dd"), "插入CONVERT_SWKC表上架部分失败");
+                    ClsErrorLogInfo.WriteSapLog("1", "CONVERT_SWKC", "ALL", DateTime.Now.ToString("yyyy-MM-dd"), "插入CONVERT_SWKC表上架部分失败--没数据");
                 }
                 string sqlSWKCDetail = @"  begin DELETE FROM CONVERT_SWKCDETAIL;
                                             INSERT INTO CONVERT_SWKCDETAIL(WERKS,WERKS_NAME,LGORT,MATNR,GESME,ERDATE,DLNAME,PMNAME,DLDATE)
